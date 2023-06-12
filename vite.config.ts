24229f059ff04f3588cf1bc897c6966ba3660240
Jude <luddensj@gmail.com>
2023-06-12 13:44:48 -0400

    including sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  css: {
+    devSourcemap: true,
+  },
   plugins: [react()],
 })
